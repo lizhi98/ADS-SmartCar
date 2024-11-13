306e709f77bb11af9442e37b763e03fa49c09387
lizhi98 <2176515832@qq.com>
2024-11-13 18:08:02 +0800

    初始提交文件

---
+++ SeekFree_TC264/user/driver/motor.c
@@ -0,0 +1,12 @@
+/*
+ * motor.c
+ *
+ *  Created on: 2024年11月10日
+ *      Author: lizhi
+ */
+#include "motor.h"
+//===========电机PWM===============
+
+void MotorPWM_init(){
+    //pwm_init();
+}
---
+++ SeekFree_TC264/user/driver/motor.h
@@ -0,0 +1,21 @@
+/*
+ * motor.h
+ *
+ *  Created on: 2024年11月10日
+ *      Author: lizhi
+ */
+
+#ifndef USER_DRIVER_MOTOR_H_
+#define USER_DRIVER_MOTOR_H_
+
+//===========头文件包含==========
+#include<zf_driver_pwm.h>
+
+//===========参数定义============
+
+#define motor0_PWM_Pin 000000000000; //电机0pwm输出引脚
+#define motor1_PWM_Pin 000000000000; //电机1pwm输出引脚
+#define motor0_PWM_fre 111111111111; //电机0频率
+#define motor1_PWM_fre 111111111111; //电机1频率
+
+#endif /* USER_DRIVER_MOTOR_H_ */
